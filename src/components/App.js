2197fc325d1bca45230b980c264c0279a02d4820
Manpreet Singh <manpreetmss400@gmail.com>
2023-11-10 20:08:14 +0530

    haha

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,96 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-import KeyValueStore from './KeyValueStore';
+import React, { useEffect } from "react";
+
+import {
+    BrowserRouter,
+    useSearchParams,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
+
+export default App;
 
 function App() {
   return (
-    <Router>
-      <div id="main">
+    <div>
+    
+      <BrowserRouter>
         <nav>
           <Link to="/">Reset</Link>
         </nav>
-        <Switch>
-          <Route path="/key-value/:keyValue" component={KeyValueStore} />
-          <Route exact path="/" component={KeyValueStore} />
-        </Switch>
-      </div>
-    </Router>
+        
+        <Routes>
+          <Route path="/key-value/:keyValue" element={<KeyValueStore />} />
+          <Route exact path="/" element={<KeyValueStore />} />
+        </Routes>
+        
+      </BrowserRouter>
+      
+    </div>
   );
 }
 
-export default App;
+function KeyValueStore(){
+  let [searchParams, setSearchParams] = useSearchParams();
+  
+  const params = [...searchParams.entries()];
+  
+  function handleChange(key, value){
+    const params = [...searchParams.entries()];
+    const newParams = params.map(pair => {
+      const k = pair[0];
+      // const val = pair[1]; // not needed
+      
+      if(k === key){
+        return [k, value];
+      }
+      
+      return pair;
+    });
+    
+    setSearchParams(newParams);
+  }
+  
+  function handleKeyDelete(e, key) {
+    e.preventDefault();
+   
+    const params = [...searchParams.entries()];
+   
+    const updatedParams = params.filter(pair => pair[0] !== key);
+   
+    setSearchParams(updatedParams);
+  }
+  
+  return (
+    <>
+      <h1>Key Value Store</h1>
+      <div>
+        {
+          params.length!==0 && params.map(([key, value]) => 
+            <div key={key} className='key-value-div'>
+              
+              <span className='key-field'>{key}: </span>
+              
+              <input
+                className='value-field' type="text"
+                value={value} onChange={e => handleChange(key, e.target.value)}
+              />
+              
+              <button
+                className='delete-btn'
+                onClick={(e) => handleKeyDelete(e, key)}
+              >delete</button>
+              
+            </div>
+          )
+        }
+        
+        {/* Do not include Update button, while mapping the key-value */}
+        { params.length>0 && <button className='update-btn'>Update Values</button> }
+      
+        { params.length==0 && <p>No key values found in URL.</p>}
+      
+      </div>
+    </>
+  );
+}
