eae01c112e88ecb79b2a1b022806b5e77663d8e8
Manpreet Singh <manpreetmss400@gmail.com>
2023-11-10 20:39:19 +0530

    abcd noob approach

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
-
+import React, { useEffect, useState } from "react";
 import {
-    BrowserRouter,
-    useSearchParams,
-    Routes,
-    Route,
-    Link,
+  BrowserRouter,
+  useSearchParams,
+  Routes,
+  Route,
+  Link,
 } from "react-router-dom";
 
 export default App;
@@ -13,83 +12,86 @@ export default App;
 function App() {
   return (
     <div>
-    
       <BrowserRouter>
         <nav>
           <Link to="/">Reset</Link>
         </nav>
         
         <Routes>
-          <Route path="/key-value/:keyValue" element={<KeyValueStore />} />
           <Route exact path="/" element={<KeyValueStore />} />
+          <Route path="/key-value/:keyValue" element={<KeyValueStore />} />
         </Routes>
-        
       </BrowserRouter>
-      
     </div>
   );
 }
 
-function KeyValueStore(){
+function KeyValueStore() {
   let [searchParams, setSearchParams] = useSearchParams();
-  
+
   const params = [...searchParams.entries()];
-  
-  function handleChange(key, value){
-    const params = [...searchParams.entries()];
-    const newParams = params.map(pair => {
-      const k = pair[0];
-      // const val = pair[1]; // not needed
-      
-      if(k === key){
-        return [k, value];
-      }
-      
-      return pair;
-    });
-    
-    setSearchParams(newParams);
+
+  // Local state to manage input values
+  const [inputValues, setInputValues] = useState(
+    Object.fromEntries(searchParams) || {}
+  );
+
+  function handleChange(key, value) {
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [key]: value,
+    }));
   }
-  
+
   function handleKeyDelete(e, key) {
     e.preventDefault();
-   
+
     const params = [...searchParams.entries()];
-   
-    const updatedParams = params.filter(pair => pair[0] !== key);
-   
+
+    const updatedParams = params.filter((pair) => pair[0] !== key);
+
     setSearchParams(updatedParams);
+    
+      // Update local state by excluding the deleted key
+    setInputValues((prevInputValues) => {
+      const { [key]: deletedKey, ...rest } = prevInputValues;
+      return rest;
+    });
   }
-  
+
+  function updateUrl() {
+    setSearchParams(Object.entries(inputValues));
+  }
+
   return (
     <>
       <h1>Key Value Store</h1>
       <div>
-        {
-          params.length!==0 && params.map(([key, value]) => 
-            <div key={key} className='key-value-div'>
-              
-              <span className='key-field'>{key}: </span>
-              
+        {params.length !== 0 &&
+          params.map(([key, value]) => (
+            <div key={key} className="key-value-div">
+              <span className="key-field">{key}: </span>
               <input
-                className='value-field' type="text"
-                value={value} onChange={e => handleChange(key, e.target.value)}
+                className="value-field"
+                type="text"
+                value={inputValues[key] || ""}
+                onChange={(e) => handleChange(key, e.target.value)}
               />
-              
               <button
-                className='delete-btn'
+                className="delete-btn"
                 onClick={(e) => handleKeyDelete(e, key)}
-              >delete</button>
-              
+              >
+                delete
+              </button>
             </div>
-          )
-        }
-        
-        {/* Do not include Update button, while mapping the key-value */}
-        { params.length>0 && <button className='update-btn'>Update Values</button> }
-      
-        { params.length==0 && <p>No key values found in URL.</p>}
-      
+          ))}
+        {/* Conditionally render the Update button */}
+        {params.length > 0 && (
+          <button className="update-btn" onClick={updateUrl}>
+            Update Values
+          </button>
+        )}
+        {params.length === 0 && <p>No key values found in URL.</p>}
       </div>
     </>
   );
